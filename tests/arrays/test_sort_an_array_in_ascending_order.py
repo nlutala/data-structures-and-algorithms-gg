dcd75de459691f98621f9e4c739f00e7ef45afc0
nlutala <nathanlutala@outlook.com>
2024-08-06 11:58:45 +0100

    Run pylint formatting on test_sort_an_arry_in_ascending_order.py

--- tests/arrays/test_sort_an_array_in_ascending_order.py
+++ tests/arrays/test_sort_an_array_in_ascending_order.py
@@ -3,6 +3,7 @@ A file test the sort_array_asc function
 
 Author: Nathan Lutala (nlutala)
 """
+
 from arrays.sort_an_array_in_ascending_order import sort_array_asc
 
 
@@ -13,7 +14,7 @@ def test_sort_array_asc():
     Output: sorted_array[] = {0, 9, 12, 14, 23}
     """
     original_array = [0, 23, 14, 12, 9]
-    
+
     # What is baffling me is that the assertion commented below
     # doesn't work. Any reason why?
     # assert sort_array_asc(original_array) == sorted(original_array)
