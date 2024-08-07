ed4c3bfdbc0341df7e8384119fd9812f4c966ecb
nlutala <nathanlutala@outlook.com>
2024-08-07 15:31:36 +0100

    Add third level 2 array problem with tests

---
+++ arrays/level2/count_pairs_with_given_sum.py
@@ -0,0 +1,40 @@
+"""
+A file to tackle the coding interview question as found on the website below:
+https://www.geeksforgeeks.org/count-pairs-with-given-sum/
+
+Author: Nathan Lutala (nlutala)
+"""
+
+
+def count_pairs_with_sum(arr: list, k: int) -> list[list[int]]:
+    """
+    Given an array of N integers, and an integer K, the task is to find the
+    number of pairs of integers in the array whose sum is equal to K. \n
+
+    :param - arr (list)\n
+
+    returns a list with the pairs that add up to the sum.
+    """
+    new_array = arr.copy()
+    pairs = []
+    freq_num = {}
+
+    for num in new_array:
+        if k - num in new_array:
+            pairs.append(sorted([k - num, num]))
+
+        if freq_num.get(str(num)) is None:
+            freq_num[str(num)] = 1
+        else:
+            freq_num[str(num)] = freq_num.get(str(num)) + 1
+
+    for i, pair in enumerate(pairs):
+        total_in_pairs = pairs.count(pair)
+        print(f"total of {pair} in pairs: {total_in_pairs}")
+        total_of_pair_1 = freq_num.get(str(pair[0]))
+        total_of_pair_2 = freq_num.get(str(pair[1]))
+        if total_in_pairs > total_of_pair_1:
+            if total_in_pairs > total_of_pair_2:
+                pairs.remove(pair)
+
+    return len(pairs)
---
+++ tests/arrays/test_count_pairs_with_given_sum.py
@@ -0,0 +1,35 @@
+"""
+A file tests the count_pairs_with_sum function
+
+Author: Nathan Lutala (nlutala)
+"""
+
+from arrays.level2.count_pairs_with_given_sum import (
+    count_pairs_with_sum,
+)
+
+
+def test_count_pairs_with_sum_example_1():
+    """
+    Example 1:
+    Input: arr[] = {1, 5, 7, -1}, K = 6
+    Output:  2
+    Explanation: Pairs with sum 6 are (1, 5) and (7, -1).
+    """
+    arr = [1, 5, 7, -1]
+    k = 6
+
+    assert count_pairs_with_sum(arr, k) == 2
+
+
+def test_count_pairs_with_sum_example_2():
+    """
+    Example 2:
+    Input: arr[] = {1, 5, 7, -1, 5}, K = 6
+    Output:  3
+    Explanation: Pairs with sum 6 are (1, 5), (7, -1) & (1, 5).
+    """
+    arr = [1, 5, 7, -1, 5]
+    k = 6
+
+    assert count_pairs_with_sum(arr, k) == 3
