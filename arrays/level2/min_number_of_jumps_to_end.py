a551c88d10a49123e545ed01602daa76534a64d2
nlutala <nathanlutala@outlook.com>
2024-09-06 17:32:33 +0100

    Trying again, but it still doesn't work :(

--- arrays/level2/min_number_of_jumps_to_end.py
+++ arrays/level2/min_number_of_jumps_to_end.py
@@ -21,27 +21,22 @@ def min_jumps(arr: list[int]) -> int:
     if len(arr_set) == 1:
         return len(arr) - 1 // arr_set[0]
     else:
-        i = 0
+        index = 0
         jumps = 0
+        current_num = arr[index]
 
-        while i < len(arr) - 1:
-            num = arr[i]
-            sub_arr = arr[i:i+num]
+        while index < len(arr) - 1:
+            sub_arr = arr[index:index+current_num+1]
 
-            sub_arr_set = list(set(sub_arr))
-
-            if len(sub_arr_set) == 1:
-                i += len(sub_arr)
-            elif len(sub_arr_set) > 1:
-                indexes_of_largest_number = [
-                    i for i in range(len(sub_arr))
-                    if sub_arr[i] == max(sub_arr)
-                ]
-                i += indexes_of_largest_number[-1] + 1
-            else:
+            if sub_arr == []:
                 return jumps
 
-            print(i, jumps, sub_arr)
-            jumps += 1
+            num_dict = {}
 
+            for i, num in enumerate(sub_arr):
+                num_dict[str(num)] = i + 1
+
+            index += num_dict.get(str(max(sub_arr)))
+            jumps += 1
+      
         return jumps
