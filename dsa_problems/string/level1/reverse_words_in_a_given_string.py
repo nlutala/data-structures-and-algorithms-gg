0ad0766f80280fe07d73697c609cfa612868347a
nlutala <nathanlutala@outlook.com>
2024-09-07 17:12:03 +0100

    Complete first level 1 string problem

--- dsa_problems/string/level1/reverse_words_in_a_given_string.py
+++ dsa_problems/string/level1/reverse_words_in_a_given_string.py
@@ -15,4 +15,33 @@ def reverse_string(string: str) -> str:
 
     returns the reversed string of the initial string.
     """
-    pass
+    # Split the string at an empty space to get the words
+    words = string.split(" ")
+    reversed_words = [words[i] for i in range(len(words) - 1, -1, -1)]
+
+    return " ".join(reversed_words)
+
+
+if __name__ == "__main__":
+
+    def test_reverse_string_example_1():
+        """
+        Example 1:
+        Input: s = "i love programming very much"
+        Output: s = "much very programming love i"
+        """
+        s = "i love programming very much"
+
+        return reverse_string(s) == "much very programming love i"
+
+    def test_reverse_string_example_2():
+        """
+        Example 2:
+        Input: s = "geeks for all"
+        Output: s = "all for geeks"
+        """
+        s = "geeks for all"
+
+        return reverse_string(s) == "all for geeks"
+
+    print(test_reverse_string_example_1(), test_reverse_string_example_2())
