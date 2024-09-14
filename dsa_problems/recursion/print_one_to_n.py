aa0a145a59f0f345598b434412c1576e75b2e974
nlutala <nathanlutala@outlook.com>
2024-09-14 17:03:03 +0100

    Add first recrusion question (not quite)

---
+++ dsa_problems/recursion/print_one_to_n.py
@@ -0,0 +1,22 @@
+"""
+A file to tackle the coding interview question as found on the website below:
+https://www.geeksforgeeks.org/print-1-to-n-without-using-loops/
+
+Author: Nathan Lutala (nlutala)
+"""
+
+
+def print_1_to_n(n: int) -> None:
+    """
+    You are given an integer N. Print numbers from 1 to N without the help of
+    loops.\n
+
+    :param - n (the number to print to)\n
+    """
+    if n > 0:
+        print(n)
+        return print_1_to_n(n - 1)
+
+
+if __name__ == "__main__":
+    print_1_to_n(5)
