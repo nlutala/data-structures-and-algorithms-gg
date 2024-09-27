8899d9435ce1bf046ba61beda5c495812c6e9df5
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-27 12:30:52 +0100

    Update test_min_number_of_jumps_to_end.py
    Add more tests and pass more tests

--- tests/arrays/level2/test_min_number_of_jumps_to_end.py
+++ tests/arrays/level2/test_min_number_of_jumps_to_end.py
@@ -11,7 +11,7 @@ def test_min_jumps_example_1():
     """
     Example 1:
     Input: arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}
-    Output: 3 (1-> 3 -> 9 -> 9)
+    Output: 3 (1 -> 3 -> 9 -> 9)
     Explanation: Jump from 1st element to 2nd element as there is only 1 step.
     Now there are three options 5, 8 or 9. If 8 or 9 is chosen then the end
     node 9 can be reached. So 3 jumps are made.
@@ -38,7 +38,7 @@ def test_min_jumps_example_3():
     Example 3 (an example I made up):
     Input:  arr[] = {1, 2, 1, 3, 1, 9, 1, 1, 1, 1, 1}
     Output: 4
-    Explanation: Jump from the 1st elemet to the second element 2. From the
+    Explanation: Jump from the 1st element to the second element 2. From the
     second element, jump to the 4th element 3. From the 4th element 3, jump to
     the 6th element 9. From the 6th element, jump to the end of the array.
     """
@@ -51,11 +51,32 @@ def test_min_jumps_example_4():
     """
     Example 4 (another example I made up):
     Input:  arr[] = {12, 2, 1, 3, 1, 9, 1, 1, 1, 1, 1}
-    Output: 4
-    Explanation: Jump from the 1st elemet to the second element 2. From the
-    second element, jump to the 4th element 3. From the 4th element 3, jump to
-    the 6th element 9. From the 6th element, jump to the end of the array.
+    Output: 1
+    Explanation: Jump from the 1st element straight to the end.
     """
     arr = [12, 2, 1, 3, 1, 9, 1, 1, 1, 1, 1]
 
     assert min_jumps(arr) == 1
+
+
+def test_min_jumps_example_5():
+    """
+    Example 5:
+    Input:  arr[] = {1, 1, 1, 1}
+    Output: 3
+    Explanation: In every step a jump is needed so the count of jumps is 3.
+    """
+    arr = [1, 1, 1, 1]
+
+    assert min_jumps(arr) == 3
+
+
+def test_min_jumps_example_6():
+    """
+    Example 6:
+    Input:  arr[] = {2, 2, 2, 2, 2}
+    Output: 2
+    """
+    arr = [2, 2, 2, 2, 2]
+
+    assert min_jumps(arr) == 2
