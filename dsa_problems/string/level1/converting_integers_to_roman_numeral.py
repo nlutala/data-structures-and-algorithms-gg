83f1b5602285b06dd02bf691330a1bec50d790c4
nlutala <nathanlutala@outlook.com>
2024-10-02 16:31:25 +0100

    Fix failing string tests

--- dsa_problems/string/level1/converting_integers_to_roman_numeral.py
+++ dsa_problems/string/level1/converting_integers_to_roman_numeral.py
@@ -1,11 +1,11 @@
 """
 A file to tackle the coding interview question as found on the website below:
-https://www.geeksforgeeks.org/roman-number-to-integer/https://www.geeksforgeeks.org/converting-decimal-number-lying-between-1-to-3999-to-roman-numerals/
+https://www.geeksforgeeks.org/converting-decimal-number-lying-between-1-to-3999-to-roman-numerals/
 Author: Nathan Lutala (nlutala)
 """
 
 
-def int_to_roman_numeral(s: str) -> int:
+def int_to_roman_numeral(s: int) -> str:
     """
     Given a number, find its corresponding Roman numeral. \n
 
@@ -13,4 +13,45 @@ def int_to_roman_numeral(s: str) -> int:
 
     returns the roman numeral form of an integer.
     """
-    pass
+    decimal_to_numeral = {
+        "1": "I",
+        "4": "IV",
+        "5": "V",
+        "9": "IX",
+        "10": "X",
+        "40": "XL",
+        "50": "L",
+        "90": "XC",
+        "100": "C",
+        "400": "CD",
+        "500": "D",
+        "900": "CM",
+        "1000": "M",
+    }
+
+    numeral = ""
+    total = s
+
+    while total != 0:
+        print(numeral, total)
+        if decimal_to_numeral.get(str(total)) is not None:
+            numeral += decimal_to_numeral.get(str(total))
+            total -= total
+        else:
+            length_total = len(str(total))
+            temp_number = 10 ** (length_total - 1) * int(str(total)[0])
+
+            if decimal_to_numeral.get(str(temp_number)) is not None:
+                numeral += decimal_to_numeral.get(str(temp_number))
+            elif temp_number < 10 ** (length_total - 1) * 4:
+                numeral += decimal_to_numeral.get(
+                    str(10 ** (length_total - 1))
+                ) * int(str(total)[0])
+            elif temp_number > 10 ** (length_total - 1) * 5:
+                numeral += decimal_to_numeral.get(
+                    str(10 ** (length_total - 1) * 5)
+                ) * (int(str(total)[0]) - 5)
+
+            total -= temp_number
+
+    return numeral
--- tests/string/level1/test_converting_integers_to_roman_numeral.py
+++ tests/string/level1/test_converting_integers_to_roman_numeral.py
@@ -34,9 +34,50 @@ def test_int_to_roman_numeral_example_2():
 def test_int_to_roman_numeral_example_3():
     """
     Example 3:
-    Input: MCMIV
-    Output: 1904
+    Input: 1904
+    Output: MCMIV
     Explanation: MCMIV is a Roman symbol which represents 1094
     """
     s = 1904
     assert int_to_roman_numeral(s) == "MCMIV"
+
+
+def test_int_to_roman_numeral_example_4():
+    """
+    Example 4:
+    Input: 3549
+    Output: MMMDXLIX
+    """
+    s = 3549
+    assert int_to_roman_numeral(s) == "MMMDXLIX"
+
+
+# Some more custom tests of my own
+def test_int_to_roman_numeral_example_5():
+    """
+    Example 5:
+    Input: 2024
+    Output: MMXXIV
+    """
+    s = 2024
+    assert int_to_roman_numeral(s) == "MMXXIV"
+
+
+def test_int_to_roman_numeral_example_6():
+    """
+    Example 6:
+    Input: 1990
+    Output: MCMXC
+    """
+    s = 1990
+    assert int_to_roman_numeral(s) == "MCMXC"
+
+
+def test_int_to_roman_numeral_example_7():
+    """
+    Example 7:
+    Input: 2000
+    Output: MM
+    """
+    s = 2000
+    assert int_to_roman_numeral(s) == "MM"
