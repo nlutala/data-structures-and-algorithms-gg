dc5eeddfa03683fe30d9da6c8f12650984e4308e
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-10-07 10:57:41 +0100

    Add ninth stack problem tests

---
+++ tests/stack/test_reverse_individual_words.py
@@ -0,0 +1,27 @@
+"""
+A file tests the reverse_string function
+
+Author: Nathan Lutala (nlutala)
+"""
+
+from dsa_problems.stack.reverse_individual_words import reverse_string
+
+
+def test_reverse_string_example_1():
+    """
+    Example 1:
+    Input : Hello World
+    Output : olleH dlroW
+    """
+    string = "Hello World"
+    assert reverse_string(string) == "olleH dlroW"
+
+
+def test_reverse_string_example_2():
+    """
+    Example 2:
+    Input :  Geeks for Geeks
+    Output : skeeG rof skeeG
+    """
+    string = "Geeks for Geeks"
+    assert reverse_string(string) == "skeeG rof skeeG"
