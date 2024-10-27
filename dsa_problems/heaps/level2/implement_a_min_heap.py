d9192357ffe062dab511c5a34e464594aa4e849d
nlutala <nathanlutala@outlook.com>
2024-10-27 09:40:12 +0000

    Fix formatting in implement_a_min_heap.py

--- dsa_problems/heaps/level2/implement_a_min_heap.py
+++ dsa_problems/heaps/level2/implement_a_min_heap.py
@@ -5,13 +5,16 @@ https://www.geeksforgeeks.org/min-heap-in-java/
 Author: Nathan Lutala (nlutala)
 """
 
-# A Min-Heap is a complete binary tree in which the value in each internal node is
-# smaller than or equal to the values in the children of that node. Mapping the elements
-# of a heap into an array is trivial: if a node is stored an index k, then its left
-# child is stored at index 2k + 1 and its right child at index 2k + 2.
-
 
 class MinHeap:
+    """
+    A Min-Heap is a complete binary tree in which the value in each internal
+    node is smaller than or equal to the values in the children of that node.
+    Mapping the elements of a heap into an array is trivial: if a node is
+    stored an index k, then its left child is stored at index 2k + 1 and its
+    right child at index 2k + 2.
+    """
+
     def __init__(self, number=None) -> None:
         self.val = number
         self.left = None
@@ -25,16 +28,16 @@ class MinHeap:
 
     def getMin(self) -> int | None:
         """
-        getMin(): It returns the root element of Min Heap. The Time Complexity of this
-        operation is O(1).
+        getMin(): It returns the root element of Min Heap. The Time Complexity
+        of this operation is O(1).
         """
         return self.val
 
     def extractMin(self) -> int:
         """
-        extractMin(): Removes the minimum element from MinHeap. The Time Complexity of
-        this Operation is O(Log n) as this operation needs to maintain the heap property
-        after removing the root.\n
+        extractMin(): Removes the minimum element from MinHeap. The Time
+        Complexity of this Operation is O(Log n) as this operation needs to
+        maintain the heap property after removing the root.\n
 
         returns the minimum element in the MinHeap.
         """
@@ -55,9 +58,10 @@ class MinHeap:
 
     def insert(self, node: int) -> None:
         """
-        insert(): Inserting a new key takes O(Log n) time. We add a new key at the end
-        of the tree. If a new key is larger than its parent, then we don’t need to do
-        anything. Otherwise, we need to traverse up to fix the violated heap property.\n
+        insert(): Inserting a new key takes O(Log n) time. We add a new key at
+        the end of the tree. If a new key is larger than its parent, then we
+        don’t need to do anything. Otherwise, we need to traverse up to fix the
+        violated heap property.\n
 
         :param - number (a MinHeap object)
         """
