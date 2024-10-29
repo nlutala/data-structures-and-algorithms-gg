fce481b02d23a60320013af9aa0a04e63d058052
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-10-29 09:41:03 +0000

    Add ninth level2 stack problem tests

---
+++ tests/stack/level2/test_stock_span_problem.py
@@ -0,0 +1,33 @@
+"""
+A file tests the stock_span function
+
+Author: Nathan Lutala (nlutala)
+"""
+
+from dsa_problems.stack.level2.stock_span_problem import stock_span
+
+
+def test_stock_span_example_1():
+    """
+    Example 1:
+    Input: N = 7, price[] = [100 80 60 70 60 75 85]
+    Output: 1 1 1 2 1 4 6
+    Explanation: Traversing the given input span for 100 will be 1, 80 is smaller than
+    100 so the span is 1, 60 is smaller than 80 so the span is 1, 70 is greater than 60
+    so the span is 2 and so on. Hence the output will be 1 1 1 2 1 4 6.
+    """
+    n, prices = 7, [100, 80, 60, 70, 60, 75, 85]
+    assert stock_span(n, prices) == [1, 1, 1, 2, 1, 4, 6]
+
+
+def test_stock_span_example_2():
+    """
+    Example 2:
+    Input: N = 6, price[] = [10 4 5 90 120 80]
+    Output:1 1 2 4 5 1
+    Explanation: Traversing the given input span for 10 will be 1, 4 is smaller than 10
+    so the span will be 1, 5 is greater than 4 so the span will be 2 and so on. Hence,
+    the output will be 1 1 2 4 5 1.
+    """
+    n, prices = 6, [10, 4, 5, 90, 120, 80]
+    assert stock_span(n, prices) == [1, 1, 2, 4, 5, 1]
