4ea13beaed85c368e73377d4d7aadd466d264cfa
nlutala <nathanlutala@outlook.com>
2024-11-09 13:13:59 +0000

    Add fourth level2 string question

---
+++ dsa_problems/string/level2/find_largest_word_in_dictionary.py
@@ -0,0 +1,28 @@
+"""
+A file to tackle the coding interview question as found on the website below:
+https://www.geeksforgeeks.org/find-largest-word-dictionary-deleting-characters-given-string/
+
+Author: Nathan Lutala (nlutala)
+"""
+
+
+def get_largest_word(dictionary: list[str], string: str) -> str:
+    """
+    Giving a dictionary and a string, find the longest string in dict which
+    can be formed by deleting some characters of the given string.\n
+
+    :param - dictionary (a list of strings)\n
+    :param - string (a string)\n
+
+    return the longest string in dict that can be formed after removing some
+    letters.
+    """
+    # Sort the words in the dictionary by longest length to shortest length
+    d = sorted(dictionary, key=len, reverse=True)
+
+    # Loop through the words in the order (defined by d) to pick out the
+    # longest word
+    for word in d:
+        largest_string = [letter for letter in word if letter in string]
+        if "".join(largest_string) in d:
+            return "".join(largest_string)
---
+++ tests/string/level2/test_find_largest_word_in_dictionary.py
@@ -0,0 +1,56 @@
+"""
+A file tests the get_largest_word function
+
+Author: Nathan Lutala (nlutala)
+"""
+
+from dsa_problems.string.level2.find_largest_word_in_dictionary import (
+    get_largest_word,
+)
+
+
+def test_get_largest_word_example_1():
+    """
+    Example 1:
+    Input : dict = {"ale", "apple", "monkey", "plea"}, str = "abpcplea"
+    Output : apple
+    """
+    dictionary, string = ["ale", "apple", "monkey", "plea"], "abpcplea"
+    assert get_largest_word(dictionary, string) == "apple"
+
+
+def test_get_largest_word_example_2():
+    """
+    Example 2:
+    Input  : dict = {"pintu", "geeksfor", "geeksgeeks", "forgeek"},
+    str = "geeksforgeeks"
+    Output : geeksgeeks
+    """
+    dictionary = ["pintu", "geeksfor", "geeksgeeks", "forgeek"]
+    string = "geeksforgeeks"
+    assert get_largest_word(dictionary, string) == "geeksgeeks"
+
+
+# Custom tests
+def test_get_largest_word_example_3():
+    """
+    Example 2:
+    Input  : dict = {"atoi", "pintu", "literally", "geeks", "forgeek"},
+    str = "literallygeeks"
+    Output : literally
+    """
+    dictionary = ["atoi", "pintu", "literally", "geeks", "forgeek"]
+    string = "literallygeeks"
+    assert get_largest_word(dictionary, string) == "literally"
+
+
+def test_get_largest_word_example_4():
+    """
+    Example 2:
+    Input  : dict = {"atoi", "pintu", "literal", "geeks", "forgeek"},
+    str = "atoialiterallygeeks"
+    Output : literal
+    """
+    dictionary = ["atoi", "pintu", "literal", "geeks", "forgeek"]
+    string = "atoialiterallygeeks"
+    assert get_largest_word(dictionary, string) == "literal"
